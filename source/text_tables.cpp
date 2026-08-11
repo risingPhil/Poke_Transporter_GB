e67cfec61fa54937e2ed4d8b5b490dc743b1ed48
Philippe Symons <philippe.symons@zappware.com>
2026-08-11 14:30:58 +0200

    Stop referencing the RSEFRLG table in PTGB, because we no longer need it there.

--- source/text_tables.cpp
+++ source/text_tables.cpp
@@ -8,14 +8,12 @@
 #include "PTGB_chunk0_lz10_bin.h"
 #include "PTGB_chunk1_lz10_bin.h"
 #include "PTGB_chunk2_lz10_bin.h"
-#include "RSEFRLG_chunk0_lz10_bin.h"
 
 static const u8 *CREDITS_chunk_list[] = {CREDITS_chunk0_lz10_bin};
 static const u8 *GB_chunk_list[] = {GB_chunk0_lz10_bin};
 static const u8 *GENERAL_chunk_list[] = {GENERAL_chunk0_lz10_bin};
 static const u8 *PKMN_NAMES_chunk_list[] = {PKMN_NAMES_chunk0_lz10_bin, PKMN_NAMES_chunk1_lz10_bin};
 static const u8 *PTGB_chunk_list[] = {PTGB_chunk0_lz10_bin, PTGB_chunk1_lz10_bin, PTGB_chunk2_lz10_bin};
-static const u8 *RSEFRLG_chunk_list[] = {RSEFRLG_chunk0_lz10_bin};
 
 void get_text_table_chunks(u32 tableIndex, const u8 ***chunkList, u32 *numChunks, u32 *chunkSize)
 {
@@ -26,11 +24,6 @@ void get_text_table_chunks(u32 tableIndex, const u8 ***chunkList, u32 *numChunks
     case PTGB_INDEX:
         *chunkList = PTGB_chunk_list;
         *numChunks = sizeof(PTGB_chunk_list) / sizeof(PTGB_chunk_list[0]);
-        break;
-    case RSEFRLG_INDEX:
-        *chunkList = RSEFRLG_chunk_list;
-        *numChunks = sizeof(RSEFRLG_chunk_list) / sizeof(RSEFRLG_chunk_list[0]);
-        *chunkSize = 4096;
         break;
     case GB_INDEX:
         *chunkList = GB_chunk_list;
